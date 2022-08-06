4f79d1f66fcaaa88ec6caea112e75e5f0f925638
Adithya Anandsaikrishnan <anandsaikrishnan.adithya@gmail.com>
2022-08-05 17:54:57 -0700

    switched to hash router for gh-pages support
    Signed-off-by: Adithya Anandsaikrishnan <anandsaikrishnan.adithya@gmail.com>

--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import App from './App';
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+      <HashRouter>
+          <Routes>
+              <Route path="/*" element={<App/>}/>
+          </Routes>
+      </HashRouter>
   );
 };
 
